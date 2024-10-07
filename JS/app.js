20dab8427b3b04847f6394cb77b11568a90a9a2c
unknown <melissa.vivas49@gmail.com>
2024-10-07 10:12:35 -0500

    Taller 2 JS

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+var edad=prompt("Escribe tu edad:")
+var nombre =prompt ("Escribe tu nombre:")
+var ciudad= "Cali"
+
+console.log (" Tu edad es: "+ edad + " Tu nombre es " + nombre + " Tu vives en: " + ciudad)
+
+alert("Tu nombre es: " + nombre + " Tu edad es: " + edad + "y vives en: " + ciudad)
